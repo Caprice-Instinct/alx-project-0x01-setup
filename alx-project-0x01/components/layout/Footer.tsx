b6c18bae8ca287c7230a1050b6f090f18da4a04e
Caprice-Instinct <linetwangui04@gmail.com>
2025-11-10 10:27:14 +0300

    components

---
+++ alx-project-0x01/components/layout/Footer.tsx
@@ -0,0 +1,10 @@
+const Footer: React.FC = () => {
+  return (
+    <footer className="flex justify-center items-center h-20 bg-gray-800 text-white">
+      <p className="text-center">
+        &copy; 2023 My Website. All rights reserved.
+      </p>
+    </footer>
+  );
+};
+export default Footer;
