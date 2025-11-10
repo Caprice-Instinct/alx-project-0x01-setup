f16f5b219ace090902ba25e6a17f4138559527fb
Caprice-Instinct <linetwangui04@gmail.com>
2025-11-10 11:17:59 +0300

    Set up navigation between pages

--- alx-project-0x01/pages/posts/index.tsx
+++ alx-project-0x01/pages/posts/index.tsx
@@ -1,6 +1,10 @@
+import Header from "@/components/layout/Header";
+
 const Post: React.FC = () => {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
+      <Header />
+
       <h1 className="text-4xl font-bold mb-4">Post</h1>
       <p className="text-lg">This is the Post page.</p>
     </div>
